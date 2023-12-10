51435a3ed6c33f9ccf9069d79de6294bb3e16f21
Kunniii <nguyenairport56@gmail.com>
2023-12-10 12:07:45 +0700

    nuke

--- src/App.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import Projects from "./views/Projects.vue";
-</script>
-
-<template>
-  <Projects></Projects>
-</template>
--- src/views/Projects.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import ProjectCard from "../components/ProjectCard.vue";
-</script>
-
-<template>
-  <div class="flex"></div>
-</template>
