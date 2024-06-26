b0d7d5432fd2a1b02bf8166bdcba78fab60814f5
Kunniii <57139436+Kunniii@users.noreply.github.com>
2024-06-27 00:12:17 +0700

    update navbar order

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,8 +4,8 @@
   <div class="bg-black w-full fixed top-0 z-10">
     <nav class="flex justify-around text-white md:w-3/4 mx-auto">
       <a href="#info">Me</a>
-      <a href="#projects">Projects</a>
       <a href="#skills">Skills</a>
+      <a href="#projects">Projects</a>
       <a href="#contact"><p class="bg-white text-black px-4 py-2 rounded-xl">Contact</p></a>
     </nav>
   </div>
